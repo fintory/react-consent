f4253cdbf4edc33ef411d3dc0ddce3e5cc9b8efe
Nick Schmidt <nick@fintory.com>
2022-11-30 15:09:20 +0100

    fix(sb): fix build of storybook for vercel

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,8 +1,18 @@
-import type { StorybookConfig } from "@storybook/core-common";
-
-const config: StorybookConfig = {
+/**
+ * @type {require("@storybook/core-common").StorybookConfig}
+ */
+const config = {
   stories: ["../stories/*.@(story.|)@(js|jsx|ts|tsx|mdx)"],
   addons: ["@storybook/addon-essentials"],
+  typescript: {
+    check: false,
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
 };
 
-export default config
+module.exports = config;
